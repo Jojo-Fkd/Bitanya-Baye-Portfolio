74a4994a40c09043e8b636bd1c236ebb66a7b530
Jonathan Fekadu <jonathanfekadu1@gmail.com>
2024-07-10 19:44:09 +0300

    committed

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* LOADING */
+
+const loading = document.querySelector(".loading");
+const body = document.querySelector("body");
+window.addEventListener("DOMContentLoaded", () => {
+  loading.style.display = "none";
+  body.style.overflow = "auto";
+});
